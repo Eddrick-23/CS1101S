f6e58f5c8ad70747216fc9ccf7d469cbac3b0f90
Eddrick <eddricklivando@gmail.com>
2026-09-17 11:32:28 +0800

    fix: edge case for PA1 B Q6

--- solutions/PA1_Practice_B/Q6.py
+++ solutions/PA1_Practice_B/Q6.py
@@ -4,7 +4,7 @@ def is_interleaving(A, B, C):
     # if one match take from that side only
     # if none match, return False
     if is_none(C):
-        return True
+        return is_none(A) and is_none(B) # in case length(C) != length(A) + length(B)
     if not is_none(A) and head(A) == head(C) and not is_none(B) and head(B) == head(C):
         return is_interleaving(tail(A), B, tail(C)) or is_interleaving(A, tail(B), tail(C))
     elif not is_none(A) and head(A) == head(C):
